2c202a1793a2bc82c661a1223d5e867d6a9d23f3
koshea9 <keegan.oshea9@gmail.com>
2021-02-01 22:00:17 -0700

    Add test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,4 +9,16 @@ class DeckTest < Minitest::Test
     deck = Deck.new(card)
     assert_instance_of Deck, deck
   end
+
+  def test_rank_at_index
+    card1 = Card.new(:diamond, "Queen", 23)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.rank_of_card_at(0)
+
+    assert_equal 12, 12
+  end
 end
