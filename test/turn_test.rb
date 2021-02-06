e95e51511f7621b296bbc4ec6651346c2e5a993f
koshea9 <keegan.oshea9@gmail.com>
2021-02-05 21:12:04 -0700

    Add Basic Turn Test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,65 +8,75 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
 
+  def setup
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+  end
+
   def test_it_exists
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
     end
 
-    def test_it_has_readable_attributes
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11)
-      card5 = Card.new(:heart, '8', 8)
-      card6 = Card.new(:diamond, 'Queen', 12)
-      card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card3, card4, card6, card7])
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-      turn = Turn.new(player1, player2)
+  def test_it_has_readable_attributes
+    assert_equal :heart, @card1.suit
+    assert_equal "Queen", @card6.value
+    assert_equal 2, @card8.rank
+    assert_equal "Megan", @player1.name
+  end
 
-      assert_equal :heart, card1.suit
-      assert_equal "Queen", card6.value
-      assert_equal 2, card8.rank
-      assert_equal "Megan", player1.name
-    end
+  def test_it_can_have_basic_type
+    @turn.player1
+    @turn.player2
 
-    def
-      test_basic_turn
-      # skip
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11)
-      card5 = Card.new(:heart, '8', 8)
-      card6 = Card.new(:diamond, 'Queen', 12)
-      card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)
-      deck1 = Deck.new([card1, card2, card5, card8])
-      deck2 = Deck.new([card3, card4, card6, card7])
-      player1 = Player.new("Megan", deck1)
-      player2 = Player.new("Aurora", deck2)
-      turn = Turn.new(player1, player2)
+    assert_equal :basic, @turn.type
+  end
 
-      turn.player1
-      turn.player2
+  def test_it_can_have_basic_winner
+    @turn.player1
+    @turn.player2
 
-      assert_equal :basic, turn.type
-    end
+    assert_equal @player1, @turn.winner
+  end
+
+  def test_it_can_pile_cards_for_basic_winner
+    @turn.player1
+    @turn.player2
+
+    @turn.pile_cards
+
+    assert_equal [@card1, @card3], @turn.spoils_of_war
+  end
+
+  def test_it_can_award_spoils_for_basic_winner
+    @turn.player1
+    @turn.player2
+
+    @turn.pile_cards
+    @turn.award_spoils(@player1)
+
+    assert_equal [@card2, @card5, @card8, @card1, @card3], @player1.deck.cards
+  end
+
+  def test_it_can_finish_basic_round
+    @turn.player1
+    @turn.player2
+
+    @turn.pile_cards
+    @turn.award_spoils(@player1)
+
+    assert_equal [@card2, @card5, @card8, @card1, @card3], @player1.deck.cards
+    assert_equal [@card4, @card6, @card7], @player2.deck.cards
+  end
+
 end
