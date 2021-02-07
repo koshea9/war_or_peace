e711321cb38f3f5187e9a4a3518cc8b5cb0d8b8f
koshea9 <keegan.oshea9@gmail.com>
2021-02-07 14:56:02 -0700

    Add Test Game Class

---
+++ test/game_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/card_generator'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/game'
+
+class GameTest < Minitest::Test
+  def test_it_exists
+    game = Game.new
+
+    assert_instance_of Game, game
+  end
+
+  def test_it_can_start
+
+  end
+
+end
